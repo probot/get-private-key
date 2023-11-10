probot/get-private-key

commit eb71777558d3f356ee4fcdd6008fffbcdddcdd5e

build: replace @pika/build with esbuild and tsc (#117)

scripts/build.mjs
@@ -1,0 +1,92 @@
+// @ts-check
+import esbuild from "esbuild";
+import { copyFile, readFile, writeFile, rm } from "fs/promises";
+import { glob } from "glob";
+
+/**
+ * @type {esbuild.BuildOptions}
+ */
+const sharedOptions = {
+  sourcemap: "external",
+  sourcesContent: true,
+  minify: false,
+  allowOverwrite: true,
+  packages: "external",
+};
+
+async function main() {
+  // Start with a clean slate
+  await rm("pkg", { recursive: true, force: true });
+  // Build the source code for a neutral platform as ESM
+  await esbuild.build({
+    entryPoints: await glob(["./src/*.ts", "./src/**/*.ts"]),
+    outdir: "pkg/dist-src",
+    bundle: false,
+    platform: "neutral",
+    format: "esm",
+    ...sharedOptions,
+    sourcemap: false,
+  });
+
+  // Remove the types file from the dist-src folder
+  const typeFiles = await glob([
+    "./pkg/dist-src/**/types.js.map",
+    "./pkg/dist-src/**/types.js",
+  ]);
+  for (const typeFile of typeFiles) {
+    await rm(typeFile);
+  }
+
+  const entryPoints = ["./pkg/dist-src/index.js"];
+
+  await Promise.all([
+    // Build the a CJS Node.js bundle
+    esbuild.build({
+      entryPoints,
+      outdir: "pkg/dist-node",
+      bundle: true,
+      platform: "node",
+      target: "node14",
+      format: "cjs",
+      ...sharedOptions,
+    }),
+    // Build an ESM browser bundle
+    esbuild.build({
+      entryPoints,
+      outdir: "pkg/dist-web",
+      bundle: true,
+      platform: "browser",
+      format: "esm",
+      ...sharedOptions,
+    }),
+  ]);
+
+  // Copy the README, LICENSE to the pkg folder
+  await copyFile("LICENSE", "pkg/LICENSE");
+  await copyFile("README.md", "pkg/README.md");
+
+  // Handle the package.json
+  let pkg = JSON.parse((await readFile("package.json", "utf8")).toString());
+  // Remove unnecessary fields from the package.json
+  delete pkg.scripts;
+  delete pkg.prettier;
+  delete pkg.release;
+  delete pkg.jest;
+  await writeFile(
+    "pkg/package.json",
+    JSON.stringify(
+      {
+        ...pkg,
+        files: ["dist-*/**", "bin/**"],
+        main: "dist-node/index.js",
+        module: "dist-web/index.js",
+        types: "dist-types/index.d.ts",
+        source: "dist-src/index.js",
+        sideEffects: false,
+      },
+      null,
+      2
+    )
+  );
+}
+main();

.github/workflows/test.yml
@@ -14,9 +14,8 @@ jobs:
     strategy:
       matrix:
         node_version:
-          - 14
-          - 16
           - 18
+          - 20
     steps:
       - uses: actions/checkout@v3
       - name: Use Node.js ${{ matrix.node_version }}
@@ -34,6 +33,6 @@ jobs:
       - uses: actions/setup-node@v3
         with:
           cache: npm
-          node-version: 16
+          node-version: 18
       - run: npm ci
       - run: npm run lint

.github/workflows/update-prettier.yml
@@ -11,7 +11,7 @@ jobs:
       - uses: actions/setup-node@v3
         with:
           cache: npm
-          node-version: 16
+          node-version: 18
       - run: npm ci
       - run: npm run lint:fix
       - uses: gr2m/create-or-update-pull-request-action@v1.x

src/index.ts
@@ -1,8 +1,6 @@
 import { resolve } from "path";
 import { existsSync, readdirSync, readFileSync } from "fs";
 
-import isBase64 from "is-base64";
-
 import { VERSION } from "./version";
 
 type Options = {
@@ -72,6 +70,10 @@ export function getPrivateKey(options: Options = {}): string | null {
     return getPrivateKey({ filepath: pemFiles[0], cwd });
   }
   return null;
+}
+
+function isBase64(str: string): boolean {
+  return Buffer.from(str, "base64").toString("base64") === str;
 }
 
 function addNewlines(privateKey: string): string {

commit 07fd1d0020982665dd598c9f78da3d96319600b0

build: Drop web builds
This package does not make sense in a browser because it depends on "fs".

This is not marked as a breaking change because the web builds were introduced by #117, which was not yet included in a release.

scripts/build.mjs
@@ -39,27 +39,15 @@ async function main() {
 
   const entryPoints = ["./pkg/dist-src/index.js"];
 
-  await Promise.all([
-    // Build the a CJS Node.js bundle
-    esbuild.build({
+  await esbuild.build({
       entryPoints,
       outdir: "pkg/dist-node",
       bundle: true,
       platform: "node",
-      target: "node14",
+      target: "node18",
       format: "cjs",
       ...sharedOptions,
-    }),
-    // Build an ESM browser bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-web",
-      bundle: true,
-      platform: "browser",
-      format: "esm",
-      ...sharedOptions,
-    }),
-  ]);
+    });
 
   // Copy the README, LICENSE to the pkg folder
   await copyFile("LICENSE", "pkg/LICENSE");
@@ -79,7 +67,6 @@ async function main() {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
         main: "dist-node/index.js",
-        module: "dist-web/index.js",
         types: "dist-types/index.d.ts",
         source: "dist-src/index.js",
         sideEffects: false,