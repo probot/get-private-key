2c4b7b2376f926e0b4a3eae9272171f13f4297ce
uzlopak <aras.abbasi@googlemail.com>
2024-07-21 02:22:16 +0200

    add types field in build package.json

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -55,6 +55,7 @@ async function main() {
       {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
+        types: "dist-types/index.d.ts",
         exports: {
           ".": {
             types: "./dist-types/index.d.ts",
